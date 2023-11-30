c8afd323c872fb275e87a0962fbb426d5cf0a00f
Tony CABAYE <tony.cabaye@smile.fr>
2023-11-30 18:19:20 +0100

    fix: styles and remove imports

---
+++ src/theme/MDXComponents.ts
@@ -0,0 +1,12 @@
+import MdxComponents from '@theme-original/MDXComponents';
+
+import Description from '@site/src/components/Description/Description';
+import Required from '@site/src/components/Required/Required';
+import StorybookEmbed from '@site/src/components/StorybookEmbed/StorybookEmbed';
+
+export default {
+  ...MdxComponents,
+  Description,
+  Required,
+  StorybookEmbed,
+};
